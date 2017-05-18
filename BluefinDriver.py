7874316c4016c46ed1463746d03973636922a6af
simmsma <simmsma@uw.edu>
2017-05-18 14:53:04 -0700

    Update BluefinDriver.py

---
+++ BluefinDriver.py
@@ -0,0 +1,26 @@
+#===============================================
+# BluefinDriver
+#
+# Matt Simms
+#===============================================
+
+"""
+import globals and classes
+"""
+
+def __Bluefin_init__():
+	"""Initialize Bluefin fishery.
+	
+	"""
+	
+def Bluefin_init(self):
+	# Fill with Albacore tuna
+	
+def Bluefin_run_sim(self):
+	# Run simulation for _____ (arbitrary unit of time)
+	
+def Bluefin_plot(self):
+	# plot graph
+	
+def Bluefin_print(self):
+	# print numerical results
