9ac912cedc9201e77812e0e3e0ff75f62b6ce3c9
simmsma <simmsma@uw.edu>
2017-05-18 14:55:15 -0700

    Update BulletDriver.py

---
+++ BulletDriver.py
@@ -0,0 +1,26 @@
+#===============================================
+# Bullet Driver
+#
+# Matt Simms
+#===============================================
+
+"""
+import globals and classes
+"""
+
+def __init__():
+	"""Initialize Bullet fishery.
+	
+	"""
+	
+def Bullet_fill(self):
+	# Fill with Bullet tuna
+	
+def Bullet_run_sim(self):
+	# Run simulation for _____ (arbitrary unit of time)
+	
+def Bullet_plot(self):
+	# plot graph
+	
+def Bullet_print(self):
+	# print numerical results
