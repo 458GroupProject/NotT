64d71a152bc69e5220a799f1d193ce8588d6a240
simmsma <simmsma@uw.edu>
2017-05-18 14:52:01 -0700

    Basic Layout

---
+++ AlbacoreDriver.py
@@ -0,0 +1,26 @@
+#===============================================
+# AlbacoreDriver
+#
+# Matt Simms
+#===============================================
+
+"""
+import globals and classes
+"""
+
+def __albacore_init__():
+	"""Initialize Albacore fishery.
+	
+	"""
+	
+def albacore_init(self):
+	# Fill with Albacore tuna
+	
+def albacore_run_sim(self):
+	# Run simulation for _____ (arbitrary unit of time)
+	
+def albacore_plot(self):
+	# plot graph
+	
+def albacore_print(self):
+	# print numerical results
