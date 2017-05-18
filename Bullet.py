c86dd147ba936241b71a5ad7523f5db91b5d8dcd
KyArthur <kyarthur.k@gmail.com>
2017-05-18 13:57:44 -0700

    Added shell

---
+++ Bullet.py
@@ -0,0 +1,14 @@
+
+class Bullet(Tuna):
+    def __init__(self, xLoc, yLoc, weight, sightRadius, state):
+        self.x = xLoc
+        self.y = yLoc
+        self.weight = weight
+        self.sightRadius = sightRadius 
+        self.state = state
+
+    def move():
+        pass
+
+    def eat():
+        pass
