84deeac1e395618816ff58d3aed524d33d44245f
KyArthur <kyarthur.k@gmail.com>
2017-05-18 13:57:57 -0700

    Added shell

---
+++ Tuna.py
@@ -0,0 +1,19 @@
+
+
+class Tuna:
+    def __init__(self, xLoc, yLoc, weight, sightRadius, state):
+        self.x = xLoc
+        self.y = yLoc
+        self.weight = weight
+        self.sightRadius = sightRadius 
+        self.state = state # 1=alive, 2=deadStarve, 3=deadSushi'd
+
+    def move():
+        pass
+
+    def eat():
+        pass
+
+
+
+
