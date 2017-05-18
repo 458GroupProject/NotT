39f3f62509e953b9bda6000b18a0098148fc0ae8
KyArthur <kyarthur.k@gmail.com>
2017-05-18 13:57:20 -0700

    Added shell

---
+++ Albacore.py
@@ -0,0 +1,14 @@
+
+class Albacore(Tuna):
+    def __init__(self, xLoc, yLoc, weight, sightRadius, state):
+        self.x = xLoc
+        self.y = yLoc
+        self.weight = weight
+        self.sightRadius = sightRadius 
+        self.state = state
+
+    def move():
+        pass
+
+    def eat(): # Less prone to cannibalism
+        pass
