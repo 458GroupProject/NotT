892555cd60491e7d27a9d410298a0384604be31d
KyArthur <kyarthur.k@gmail.com>
2017-05-21 12:23:37 -0700

    Changed class variables

--- Tuna.py
+++ Tuna.py
@@ -1,17 +1,33 @@
+#=======================================================================
+#                        General Documentation
 
+""" Allows the creation and manipulation of a Tuna object.
+"""
+
+#-----------------------------------------------------------------------
+#                       Additional Documentation
+# Modification History:
+# - 18 May 2017:  Original by Arthur (Ky-Phuoc) Khuu
+#
+# Notes:
+# - Written for Python 3
+# - See import statements throughout for more information on non-
+#   built-in packages and modules required.
+#
+#=======================================================================
 
 class Tuna:
-    def __init__(self, xLoc, yLoc, weight, sightRadius, state):
+    def __init__(self, xLoc, yLoc, weight, sightRadius=1, energy=0.5):
         self.x = xLoc
         self.y = yLoc
         self.weight = weight
         self.sightRadius = sightRadius 
-        self.state = state # 1=alive, 2=deadStarve, 3=deadSushi'd
+        self.energy = energy
 
     def move(environment):
         pass
 
-    def eat(environment, amount):
+    def eat(amount):
         pass
 
 
