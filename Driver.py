5e5ad36bcf264842cb6297d7f07cd720b5b839f9
jgnewton <jeremygnewton@gmail.com>
2017-05-22 19:56:30 -0700

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -9,9 +9,28 @@ import Globalvars.py
 Initializes the grid with water agents, then adds food and prey according
 to starting conditions,
 also adds tuna larvae
+
+Starting population will not be exact, but the target init popualtion/ grid area
+will be the liklihookd of a larva starting in a certain cell
 """
 def init():
-    pass
+            Global grid, numAlive
+    #creating h by w water grid
+    simplelist = [water() for w in xrange(42*42)]    
+    grid=N.array(simplelist)
+    grid=N.reshape(grid, (42,42)     
+            
+            
+    #loop through rows along height        
+    for r in range(tankh):
+            #loop through columns in width
+            for c in range(tankw):
+                   r = N.random.uniform()
+                   if r > initPop/(tankw*tankh):
+                        t = tuna()
+                        tuna.append(t)
+                        grid[r][c].tuna=true
+                        
 
 """
 Loops through all tuna agents and calls tunaMayEat()
