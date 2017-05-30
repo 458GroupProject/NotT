39f292906e6713547ecf44fd685b9e26312dd4cb
tien.ng.huynh <tien.ng.huynh@gmail.com>
2017-05-29 23:13:39 -0700

    Fixed Analysis

--- Analysis.py
+++ Analysis.py
@@ -11,6 +11,14 @@ class analysis:
         self.runs = rn + 1
         for i in range(self.runs):
             exec("self.arr_data_" + str(i) + " = N.empty([8,self.iterations],dtype='d')")
+
+        self.final_arr_numAlive_data = N.empty([self.runs],dtype='d')
+        self.final_arr_numStarved_data = N.empty([self.runs],dtype='d')
+        self.final_arr_numCorpses_data = N.empty([self.runs],dtype='d')
+        self.final_arr_numEatenAlive_data = N.empty([self.runs],dtype='d')
+        self.final_arr_numCorpsesEaten_data = N.empty([self.runs],dtype='d')
+        self.final_arr_avgLength_data = N.empty([self.runs],dtype='d')
+        self.final_arr_avgEnergy_data = N.empty([self.runs],dtype='d')
         
     def graph(self, run, arr_iterations, arr_numAlive, arr_numStarved, arr_numCorpses, arr_numEatenAlive, arr_numCorpsesEaten, arr_avgLength, arr_avgEnergy):
         plt.figure(run)
@@ -32,43 +40,28 @@ class analysis:
         plt.show()
         
     def collect(self, run, arr_iterations, arr_numAlive, arr_numStarved, arr_numCorpses, arr_numEatenAlive, arr_numCorpsesEaten, arr_avgLength, arr_avgEnergy):
-        exec("self.arr_data_" + str(run) + "[0,:] = arr_iterations")
-        exec("self.arr_data_" + str(run) + "[1,:] = arr_numAlive")
-        exec("self.arr_data_" + str(run) + "[2,:] = arr_numStarved")
-        exec("self.arr_data_" + str(run) + "[3,:] = arr_numCorpses")
-        exec("self.arr_data_" + str(run) + "[4,:] = arr_numEatenAlive")
-        exec("self.arr_data_" + str(run) + "[5,:] = arr_numCorpsesEaten")
-        exec("self.arr_data_" + str(run) + "[6,:] = arr_avgLength")
-        exec("self.arr_data_" + str(run) + "[7,:] = arr_avgEnergy")
-            
+        self.final_arr_numAlive_data[run] = arr_numAlive[-1]
+        self.final_arr_numStarved_data[run] = arr_numStarved[-1]
+        self.final_arr_numCorpses_data[run] = arr_numCorpses[-1]
+        self.final_arr_numEatenAlive_data[run] = arr_numEatenAlive[-1] 
+        self.final_arr_numCorpsesEaten_data[run] = arr_numCorpsesEaten[-1]
+        self.final_arr_avgLength_data[run] = arr_avgLength[-1]
+        self.final_arr_avgEnergy_data[run] = arr_avgEnergy[-1]
+        
+        
     def analyze(self):
-        final_arr_numAlive_data = N.empty([self.runs],dtype='d')
-        final_arr_numStarved_data = N.empty([self.runs],dtype='d')
-        final_arr_numCorpses_data = N.empty([self.runs],dtype='d')
-        final_arr_numEatenAlive_data = N.empty([self.runs],dtype='d')
-        final_arr_numCorpsesEaten_data = N.empty([self.runs],dtype='d')
-        final_arr_avgLength_data = N.empty([self.runs],dtype='d')
-        final_arr_avgEnergy_data = N.empty([self.runs],dtype='d')
-        for i in range(self.runs):
-            exec("final_arr_numAlive_data = N.append(final_arr_numAlive_data[i],self.arr_data_" + str(i) + "[1,-1])")
-            exec("final_arr_numStarved_data = N.append(final_arr_numStarved_data[i],self.arr_data_" + str(i) + "[2,-1])")
-            exec("final_arr_numCorpses_data = N.append(final_arr_numCorpses_data[i],self.arr_data_" + str(i) + "[3,-1])")
-            exec("final_arr_numEatenAlive_data = N.append(final_arr_numEatenAlive_data[i],self.arr_data_" + str(i) + "[4,-1])")
-            exec("final_arr_numCorpsesEaten_data = N.append(final_arr_numCorpsesEaten_data[i],self.arr_data_" + str(i) + "[5,-1])")
-            exec("final_arr_avgLength_data = N.append(final_arr_avgLength_data[i],self.arr_data_" + str(i) + "[6,-1])")
-            exec("final_arr_avgEnergy_data = N.append(final_arr_avgEnergy_data[i],self.arr_data_" + str(i) + "[7,-1])")
-        final_numAlive_avg = N.average(final_arr_numAlive_data)
+        final_numAlive_avg = N.average(self.final_arr_numAlive_data)
         print("Average Final Alive: " + str(final_numAlive_avg))
-        final_numStarved_avg = N.average(final_arr_numStarved_data)
+        final_numStarved_avg = N.average(self.final_arr_numStarved_data)
         print("Average Final Starved: " + str(final_numStarved_avg))
-        final_numCorpses_avg = N.average(final_arr_numCorpses_data)
+        final_numCorpses_avg = N.average(self.final_arr_numCorpses_data)
         print("Average Final Corpses: " + str(final_numCorpses_avg))
-        final_numEatenAlive_avg = N.average(final_arr_numEatenAlive_data)
+        final_numEatenAlive_avg = N.average(self.final_arr_numEatenAlive_data)
         print("Average Final Eaten Alive: " + str(final_numEatenAlive_avg))
-        final_numCorpsesEaten_avg = N.average(final_arr_numCorpsesEaten_data)
+        final_numCorpsesEaten_avg = N.average(self.final_arr_numCorpsesEaten_data)
         print("Average Final Corpses Eaten: " + str(final_numCorpsesEaten_avg))
-        final_avgLength_avg = N.average(final_arr_avgLength_data)
+        final_avgLength_avg = N.average(self.final_arr_avgLength_data)
         print("Average Final Length: " + str(final_avgLength_avg))
-        final_avgEnergy_avg = N.average(final_arr_avgEnergy_data)
+        final_avgEnergy_avg = N.average(self.final_arr_avgEnergy_data)
         print("Average Final Energy: " + str(final_avgEnergy_avg))
         
