0184761831f8e8452431c909766d457e9384718c
jgnewton <jeremygnewton@gmail.com>
2017-05-30 00:46:02 -0700

    upload goddamnit

--- Analysis.py
+++ Analysis.py
@@ -9,9 +9,7 @@ class analysis:
     def __init__(self,it, rn):
         self.iterations = it + 1
         self.runs = rn + 1
-        for i in range(self.runs):
-            exec("self.arr_data_" + str(i) + " = N.empty([8,self.iterations],dtype='d')")
-
+        
         self.final_arr_numAlive_data = N.empty([self.runs],dtype='d')
         self.final_arr_numStarved_data = N.empty([self.runs],dtype='d')
         self.final_arr_numCorpses_data = N.empty([self.runs],dtype='d')
@@ -26,10 +24,10 @@ class analysis:
         plt.ylabel('Number')
         plt.plot(arr_iterations,arr_numAlive,'b.',label='Alive')
         plt.plot(arr_iterations,arr_numStarved,'g.',label='Starved')
-        plt.plot(arr_iterations,arr_numCorpses,'k.',label='Corpses')
+        #plt.plot(arr_iterations,arr_numCorpses,'k.',label='Corpses')
         plt.plot(arr_iterations,arr_numEatenAlive,'c.',label='EatenAlive')
-        plt.plot(arr_iterations,arr_numCorpsesEaten,'r.',label='CorpsesEaten')
-        plt.legend(loc='lower left', bbox_to_anchor=(0., 1.02, 1., .102),ncol=5)
+        #plt.plot(arr_iterations,arr_numCorpsesEaten,'r.',label='CorpsesEaten')
+        plt.legend(loc='lower left', bbox_to_anchor=(0., 1.02, 1., .102),ncol=3)
         plt.subplot(312)
         plt.ylabel('Average Length')
         plt.plot(arr_iterations,arr_avgLength,'k.')
@@ -54,14 +52,17 @@ class analysis:
         print("Average Final Alive: " + str(final_numAlive_avg))
         final_numStarved_avg = N.average(self.final_arr_numStarved_data)
         print("Average Final Starved: " + str(final_numStarved_avg))
-        final_numCorpses_avg = N.average(self.final_arr_numCorpses_data)
-        print("Average Final Corpses: " + str(final_numCorpses_avg))
+        #final_numCorpses_avg = N.average(self.final_arr_numCorpses_data)
+        #print("Average Final Corpses: " + str(final_numCorpses_avg))
         final_numEatenAlive_avg = N.average(self.final_arr_numEatenAlive_data)
         print("Average Final Eaten Alive: " + str(final_numEatenAlive_avg))
-        final_numCorpsesEaten_avg = N.average(self.final_arr_numCorpsesEaten_data)
-        print("Average Final Corpses Eaten: " + str(final_numCorpsesEaten_avg))
+        #final_numCorpsesEaten_avg = N.average(self.final_arr_numCorpsesEaten_data)
+        #print("Average Final Corpses Eaten: " + str(final_numCorpsesEaten_avg))
         final_avgLength_avg = N.average(self.final_arr_avgLength_data)
         print("Average Final Length: " + str(final_avgLength_avg))
         final_avgEnergy_avg = N.average(self.final_arr_avgEnergy_data)
         print("Average Final Energy: " + str(final_avgEnergy_avg))
+        
+        final = (final_numAlive_avg, final_numStarved_avg, final_numEatenAlive_avg, final_avgLength_avg, final_avgEnergy_avg)
+        return final
         
