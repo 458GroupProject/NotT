0632ce51697e8e1af7fc521809e1d768a85145da
KyArthur <kyarthur.k@gmail.com>
2017-05-30 15:11:35 -0700

    Fixed Formatting

--- Analysis.py
+++ Analysis.py
@@ -10,31 +10,33 @@ class analysis:
         self.iterations = it
         self.runs = rn
         
-        self.final_arr_numAlive_data = N.empty([self.runs],dtype='d')
-        self.final_arr_numStarved_data = N.empty([self.runs],dtype='d')
-        self.final_arr_numCorpses_data = N.empty([self.runs],dtype='d')
-        self.final_arr_numEatenAlive_data = N.empty([self.runs],dtype='d')
-        self.final_arr_numCorpsesEaten_data = N.empty([self.runs],dtype='d')
-        self.final_arr_avgLength_data = N.empty([self.runs],dtype='d')
-        self.final_arr_avgEnergy_data = N.empty([self.runs],dtype='d')
-        
+        self.final_arr_numAlive_data = N.empty([self.runs], dtype='d')
+        self.final_arr_numStarved_data = N.empty([self.runs], dtype='d')
+        self.final_arr_numCorpses_data = N.empty([self.runs], dtype='d')
+        self.final_arr_numEatenAlive_data = N.empty([self.runs], dtype='d')
+        self.final_arr_numCorpsesEaten_data = N.empty([self.runs], dtype='d')
+        self.final_arr_avgLength_data = N.empty([self.runs], dtype='d')
+        self.final_arr_avgEnergy_data = N.empty([self.runs], dtype='d')
+    
+    # Create graph for simulation
     def graph(self, run, arr_iterations, arr_numAlive, arr_numStarved, arr_numCorpses, arr_numEatenAlive, arr_numCorpsesEaten, arr_avgLength, arr_avgEnergy):
         plt.figure(run)
         plt.subplot(311)
         plt.ylabel('Number')
-        plt.plot(arr_iterations,arr_numAlive,'b.',label='Alive')
-        plt.plot(arr_iterations,arr_numStarved,'g.',label='Starved')
-        plt.plot(arr_iterations,arr_numEatenAlive,'c.',label='EatenAlive')
-        plt.legend(loc='lower left', bbox_to_anchor=(0., 1.02, 1., .102),ncol=3)
+        plt.plot(arr_iterations,arr_numAlive, 'b.', label='Alive')
+        plt.plot(arr_iterations,arr_numStarved, 'g.', label='Starved')
+        plt.plot(arr_iterations,arr_numEatenAlive, 'c.', label='EatenAlive')
+        plt.legend(loc='lower left', bbox_to_anchor=(0., 1.02, 1., 0.102), ncol=3)
         plt.subplot(312)
         plt.ylabel('Average Length')
-        plt.plot(arr_iterations,arr_avgLength,'k.')
+        plt.plot(arr_iterations, arr_avgLength,'k.')
         plt.subplot(313)
         plt.xlabel('Iteration')
         plt.ylabel('Average Energy')
-        plt.plot(arr_iterations,arr_avgEnergy,'k.')
+        plt.plot(arr_iterations, arr_avgEnergy,'k.')
         plt.show()
-        
+    
+    # Get data for variables
     def collect(self, run, arr_iterations, arr_numAlive, arr_numStarved, arr_numCorpses, arr_numEatenAlive, arr_numCorpsesEaten, arr_avgLength, arr_avgEnergy):
         self.final_arr_numAlive_data[run] = arr_numAlive[-1]
         self.final_arr_numStarved_data[run] = arr_numStarved[-1]
@@ -44,7 +46,7 @@ class analysis:
         self.final_arr_avgLength_data[run] = arr_avgLength[-1]
         self.final_arr_avgEnergy_data[run] = arr_avgEnergy[-1]
         
-        
+    # Print and return average for each variable  
     def analyze(self):
         final_numAlive_avg = N.average(self.final_arr_numAlive_data)
         print("Average Final Alive: " + str(final_numAlive_avg))
